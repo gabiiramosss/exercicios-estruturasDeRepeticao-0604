26e9784ecc2c45ea22ce5b29b877e8c4ac88ba3a
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-06 09:29:02 -0300

    Exercício 1 concluído

---
+++ exercicio-1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Faça um programa que peça uma nota, entre 0 e 10. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+//Do While
+
+float nota;
+
+do
+{
+    Console.WriteLine($"Insira sua nota: ");
+    nota = float.Parse(Console.ReadLine());
+
+    if (nota > 10 || nota < 0)
+    {
+        Console.WriteLine($"Número inválido, insira novamente.");
+    }
+} while (nota > 10 || nota < 0);
