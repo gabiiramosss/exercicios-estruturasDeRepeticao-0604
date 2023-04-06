0ea471f41417ba90de9dde8298baae44e83af5b2
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-06 09:49:48 -0300

    Exercício 2 concluído

---
+++ exercicio-2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+//Do While
+
+ string usuario;
+ string senha;
+
+ do
+ {
+     Console.WriteLine($"Insira seu usuário: ");
+     usuario = Console.ReadLine();
+
+     Console.WriteLine($"Insira sua senha: ");
+     senha = Console.ReadLine();
+
+     if (usuario == senha)
+     {
+         Console.WriteLine($"Revise seus dados e insira-os novamente!");
+     }
+ }  while (usuario == senha);
