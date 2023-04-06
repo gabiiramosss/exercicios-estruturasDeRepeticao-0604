a411b89fdae4aa428d32fcc969755c16414b332b
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-06 09:50:38 -0300

    Exercício 3 concluído

---
+++ exercicio-3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+//For
+Console.WriteLine($"Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+{
+    Console.WriteLine($"{numero} * {multiplicador} = {numero * multiplicador}");
+}
+ 
